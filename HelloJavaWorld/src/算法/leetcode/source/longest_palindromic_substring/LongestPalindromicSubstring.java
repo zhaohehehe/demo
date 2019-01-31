ed7760b4dd08507f4e45a6ad3b264b593d99148a
zhaohe <zhaohe@bonc.com.cn>
2019-01-31 11:25:57 +0800

    update

--- HelloJavaWorld/src/算法/leetcode/source/longest_palindromic_substring/LongestPalindromicSubstring.java
+++ HelloJavaWorld/src/算法/leetcode/source/longest_palindromic_substring/LongestPalindromicSubstring.java
@@ -11,6 +11,12 @@ public class LongestPalindromicSubstring {
 		System.out.println(LongestPalindromicSubstring.myLongestPalindrome("aaa"));
 		System.out.println(LongestPalindromicSubstring.myLongestPalindrome("babad"));
 		System.out.println(LongestPalindromicSubstring.myLongestPalindrome("abcda4adcba"));
+
+		System.out.println(LongestPalindromicSubstring.leetcodeLongestPalindrome(""));
+		System.out.println(LongestPalindromicSubstring.leetcodeLongestPalindrome("abb"));
+		System.out.println(LongestPalindromicSubstring.leetcodeLongestPalindrome("aaa"));
+		System.out.println(LongestPalindromicSubstring.leetcodeLongestPalindrome("babad"));
+		System.out.println(LongestPalindromicSubstring.leetcodeLongestPalindrome("abcda4adcba"));
 	}
 
 	/**
@@ -87,5 +93,44 @@ public class LongestPalindromicSubstring {
 		}
 		return true;
 
+	}
+
+	/**
+	 * 时间复杂度：O(n*n)
+	 * 
+	 * We observe that a palindrome(回文) mirrors around its center. Therefore, a
+	 * palindrome can be expanded from its center, and there are only 2n-1 such
+	 * centers. You might be asking why there are 2n-1 but not n centers? The
+	 * reason is the center of a palindrome can be in between two letters. Such
+	 * palindromes have even number of letters (such as "abba") and its center
+	 * are between the two 'b's.
+	 * 
+	 * @param s
+	 * @return
+	 */
+
+	public static String leetcodeLongestPalindrome(String s) {
+		if (s == null || s.length() < 1)
+			return "";
+		int start = 0, end = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int len1 = expandAroundCenter(s, i, i);
+			int len2 = expandAroundCenter(s, i, i + 1);
+			int len = Math.max(len1, len2);
+			if (len > end - start) {
+				start = i - (len - 1) / 2;
+				end = i + len / 2;
+			}
+		}
+		return s.substring(start, end + 1);
+	}
+
+	private static int expandAroundCenter(String s, int left, int right) {
+		int L = left, R = right;
+		while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+			L--;
+			R++;
+		}
+		return R - L - 1;// (R-1)-(L+1)+1 = R-1-L-1+1=R-L-1
 	}
 }
