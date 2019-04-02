bb256cc3d983a2c2fb081ab3c8908dd315bf695a
zhaohe <zhaohe@bonc.com.cn>
2019-04-02 11:51:58 +0800

    delete test

--- HelloJavaWorld/HelloJNI.c
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <stdio.h>
-#include "HelloJNI.h"
-
-// Implementation of native method sayHello() of HelloJNI class
-JNIEXPORT void JNICALL Java_HelloJNI_sayHello(JNIEnv *env, jobject thisObj, jstring name){
- printf("Hello %s \n", name );
-   return;
-}
