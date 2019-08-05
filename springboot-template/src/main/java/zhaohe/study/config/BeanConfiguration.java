bf263c6a12622c7034733e83d2d589b6838ad505
赵鹤 <zhaohe@bonc.com.cn>
2019-08-05 19:17:35 +0800

    加载配置文件

--- springboot-template/src/main/java/zhaohe/study/config/BeanConfiguration.java
+++ springboot-template/src/main/java/zhaohe/study/config/BeanConfiguration.java
@@ -1,6 +1,5 @@
 package zhaohe.study.config;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,8 +7,8 @@ import java.util.List;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
 public class BeanConfiguration {
@@ -32,6 +31,7 @@ public class BeanConfiguration {
 		return new SpringContextUtil();
 	}
 	
+	/*
 	@Bean("MyPropertyPlaceholderConfigurer")
 	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
 		final MyPropertyPlaceholderConfigurer ppc = new MyPropertyPlaceholderConfigurer();
@@ -42,7 +42,7 @@ public class BeanConfiguration {
 			File file = new ClassPathResource(configPath).getFile();
 			String[] resources = file.list();
 			for (String resource : resources) {
-				if (true) {//resource.startsWith("b") || "database.properties".equalsIgnoreCase(resource)) {
+				if (resource.startsWith("b") || "database.properties".equalsIgnoreCase(resource)) {
 					resource = configPath + "/" + resource;
 					resourceList.add(new ClassPathResource(resource));
 				}
@@ -53,5 +53,35 @@ public class BeanConfiguration {
 		ppc.setLocations(resourceList.toArray(new Resource[] {}));
 		ppc.setFileEncoding("UTF-8");
 		return ppc;
+	}*/
+	/**
+	 * <pre>
+	 * 上述Bean中的方法在Eclipse中测试没问题，但是部署JAR包之后会找不到配置文件，因为getFile()不能从JAR包中读取文件：
+	 * java.io.FileNotFoundException: 
+	 * class path resource [config] cannot be resolved to absolute file path because it does not reside in the file 
+	 * system: jar:file:/*.jar!/BOOT-INF/classes!/config
+	 * 所以使用PathMatchingResourcePatternResolver读取</pre>
+	 * @return
+	 */
+	@Bean("MyPropertyPlaceholderConfigurer")
+	public PropertyPlaceholderConfigurer customPropertyPlaceholderConfigurer() {
+		final MyPropertyPlaceholderConfigurer ppc = new MyPropertyPlaceholderConfigurer();
+		final List<Resource> resourceList = new ArrayList<>();
+
+		PathMatchingResourcePatternResolver resourceLoader = new PathMatchingResourcePatternResolver();
+		Resource dbResource = resourceLoader.getResource("classpath:/config/database.properties");
+		resourceList.add(dbResource);
+		try {
+			Resource[] resources = resourceLoader.getResources("classpath:/config/test/*.properties");
+			for (Resource resource : resources) {
+				resourceList.add(resource);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		ppc.setLocations(resourceList.toArray(new Resource[] {}));
+		ppc.setFileEncoding("UTF-8");
+		return ppc;
+
 	}
 }
